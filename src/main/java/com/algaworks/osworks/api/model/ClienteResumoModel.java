24fb9a844aede2fb85f81fe925d910d418c763b8
fabricio-alvares <fabricioalvaresoliveira@gmail.com>
2020-09-18 18:49:16 -0300

    first commit

---
+++ src/main/java/com/algaworks/osworks/api/model/ClienteResumoModel.java
@@ -0,0 +1,21 @@
+package com.algaworks.osworks.api.model;
+
+public class ClienteResumoModel {
+	
+	private Long id;
+	private String nomeCliente;
+	
+	
+	public Long getId() {
+		return id;
+	}
+	public void setId(Long id) {
+		this.id = id;
+	}
+	public String getNomeCliente() {
+		return nomeCliente;
+	}
+	public void setNomeCliente(String nomeCliente) {
+		this.nomeCliente = nomeCliente;
+	}
+}
---
+++ src/main/java/com/algaworks/osworks/domain/ValidationGroups.java
@@ -0,0 +1,11 @@
+package com.algaworks.osworks.domain;
+
+public interface ValidationGroups {
+	
+	public interface ClienteId{
+		
+		// Com o representarion model, não é mais necessário
+		
+	}
+
+}
